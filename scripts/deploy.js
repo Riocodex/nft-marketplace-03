89474507fda3416afb8802c5bea5b706ea9a8ea8
Riocodex <onwukachibike@gmail.com>
2022-11-08 03:29:17 +0100

    updated

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,59 +1,18 @@
+const hre = require ("hardhat")
 
-const hre = require("hardhat");
+async function main(){
+  const Lock = await hre.ethers.getContractFactory("Lock")
+  const lock = await lock.deploy()
 
-async function main() {
-//connecting the contract 
-  const VotingContractFactory = await hre.ethers.getContractFactory("Voting");
-  const votingContract = await VotingContractFactory.deploy();
-  await votingContract.deployed();
-//console.logging the value
-  console.log("VotingContract deployed to:", votingContract.address);
+  await lock.deployed();
 
-  //operations
-//statevariables
+  console.log(
+    `Lock with 1 ETH and unlock with timestamp ${unlockTime} deployed to ${lock.address}`
+  );
 
-const money = {value: hre.ethers.utils.parseEther("2")};
-//candidate accounts
-const [owner, tipper, tipper2, tipper3 , tipper4 ] = await hre.ethers.getSigners();
-
-//voters accounts
-const [voter1 , voter2 ,voter3 , voter4] = await hre.ethers.getSigners();
-
-//start election
-await votingContract.connect(tipper).startElection('deployer' , 20 , 2000 , money);
-
-//register as candidates
-await votingContract.connect(tipper2).register('rio' , money);
-await votingContract.connect(tipper3).register('patrick' , money);
-await votingContract.connect(tipper4).register('king' , money);
-
-//voting operation
-await votingContract.connect(voter1).vote('rio' , money);
-await votingContract.connect(voter2).vote('rio' , money);
-await votingContract.connect(voter3).vote('patrick' , money);
-await votingContract.connect(voter4).vote('king' , money);
-
-//deciding winner
-await votingContract.decideWinner();
-
-
-
-//return candidates
-let candidates = await votingContract.getCandidates();
-console.log("candidates for the election are: ", candidates);
-
-//return winner
-let winner = await votingContract.viewWinner();
-console.log("and the winner of this election is...." , winner);
-// return electionDetails
-let returnElectionDetails = await votingContract.getElectionDetails();
-console.log('Election Details are: ', returnElectionDetails);
 }
 
-
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
